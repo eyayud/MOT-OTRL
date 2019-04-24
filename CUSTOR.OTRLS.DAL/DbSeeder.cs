45bfd69972494f8368b4097df13a6125cec6a4aa
eyayut <eyayustar1@gmail.com>
2019-04-24 10:09:18 +0300

    removed identity reference

--- CUSTOR.OTRLS.DAL/DbSeeder.cs
+++ CUSTOR.OTRLS.DAL/DbSeeder.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace CUSTOR.OTRLS.Core
